ecb7f632f54bb332b0095ff40dc1bbde7bfe6c0a
Jules MAISONNEUVE <jumaison@e3r5p16.clusters.42paris.fr>
2022-05-08 17:49:05 +0200

    ex00 inc

---
+++ ex02/Base.cpp
@@ -0,0 +1,76 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base::~Base(void)
+{
+    return ;
+}
+
+Base * generate(void)
+{
+    Base *base;
+    srand (time(NULL));
+    int res = (rand() % 3);
+    std::cout << res << std::endl;
+    switch (res)
+    {
+    case 0:
+        std::cout << "Creating A class" << std::endl;
+        base = new A();
+        break ;
+    case 1:
+        std::cout << "Creating B class" << std::endl;
+        base = new B();
+        break ;
+    case 2:
+        std::cout << "Creating C class" << std::endl;
+        base = new C();
+        break ;
+    }
+    return (base);
+}
+
+void identify(Base *p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "Class A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "Class B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "Class C" << std::endl;
+    else 
+        std::cout << "Unknown type" << std::endl;
+}
+
+void identify(Base& p)
+{
+    try
+    {
+        (void)dynamic_cast<A&>(p);
+         std::cout << "Class A" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        // std::cerr << e.what() << '\n';
+    }
+     try
+    {
+        (void)dynamic_cast<B&>(p);
+         std::cout << "Class B" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        // std::cerr << e.what() << '\n';
+    }
+     try
+    {
+        (void)dynamic_cast<C&>(p);
+         std::cout << "Class C" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        // std::cerr << e.what() << '\n';
+    }
+}
---
+++ ex02/main.cpp
@@ -0,0 +1,13 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+int main(void)
+{
+    Base *base;
+    base = generate();
+    identify(*base);
+    identify(base);
+    return 0;
+}
