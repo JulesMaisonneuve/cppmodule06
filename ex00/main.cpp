1a67fd17b57527285c671437a1d9294a4b6f6f72
Jules Maisonneuve <jumaison@student.42.fr>
2022-05-09 05:55:51 +0200

    ex00 soon

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -8,14 +8,16 @@ int getType(const std::string value)
     if (value.empty())
     {
         std::cout << "Error: empty string" << std::endl;
-        return UNKNOWN;
+        return (UNKNOWN);
     }
     else if (value == "-inf" || value == "-inff")
-		return MINFINITY;
+		return (MINFINITY);
 	else if (value == "inf" || value == "inff")
-		return INFINITY;
+		return (INFINITY);
+        else if (value == "+inf" || value == "+inff")
+		return (INFINITY);
 	else if (!isdigit(value.at(0)) && value.size() == 1)
-		return CHAR;
+		return (CHAR);
     if (value.at(0) == '-' || isdigit(value.at(0)))
 	{
 		i = 1;
@@ -23,7 +25,7 @@ int getType(const std::string value)
 			i++;
 		if (i == value.size())
         {
-			return INT;
+			return (INT);
         }
 		i = 1;
 		while (has_dot <= 1 && i < value.size())
@@ -31,32 +33,41 @@ int getType(const std::string value)
 			if (value[i] == '.')
 				has_dot++;
 			if (!isdigit(value[i]) && value[i] != '.' && (value[i] != 'f' || i != value.size() - 1))
-				return NAN;
+				return (NAN);
 			i++;
 		}
 	}
 	else
-		return NAN;
+		return (NAN);
 	if (has_dot > 1)
-		return NAN;
-	return (value.at(value.size() - 1) == 'f') ? FLOAT : DOUBLE;
+		return (NAN);
+	return ((value.at(value.size() - 1) == 'f') ? FLOAT : DOUBLE);
 }
 
+bool has_only_digits(std::string str)
+{
+   std::string::iterator iter;
+
+	for (iter = str.begin(); iter != str.end(); ++iter)
+		if (!std::isdigit(*iter))
+			return (false);
+	return (true);
+}
+
+bool is_int(std::string str)
+{
+    if (str.find('.', 0) != std::string::npos && !has_only_digits(str))
+        return (false);
+    return (true);
+}
 
 std::string print_char(int data_type, double data)
 {
     std::string str;
     char c;
 
-    // if (data_type == CHAR)
-    // {
-    //     str = "'";
-    //     str += value;
-    //     str += "'";
-    //     return (str);
-    // }
     c = static_cast<char>(data);
-    if (data != c || data_type == UNKNOWN || data_type == MINFINITY || data_type == INFINITY || data_type == NAN)
+    if (data_type == UNKNOWN || data_type == MINFINITY || data_type == INFINITY || data_type == NAN)
         return ("impossible");
     if (!std::isprint(c))
         return ("Non displayable");
@@ -66,22 +77,21 @@ std::string print_char(int data_type, double data)
     return (str);
 }
 
-std::string print_double(int data_type, std::string value, double data)
+std::string print_double(int data_type, double data)
 {
     std::ostringstream ostr;
     std::string res;
 
     ostr << data;
     res = ostr.str();
-    if (data_type == DOUBLE)
-		return (value);
-	else if (data_type == NAN)
+	if (data_type == NAN)
 		return ("nan");
 	else if (data_type == INFINITY)
 		return ("inf");
 	else if (data_type == MINFINITY)
 		return ("-inf");
-    res += ".0";
+    if (is_int(res))
+        res += ".0";
     return (res);
 }
 
@@ -93,39 +103,35 @@ std::string print_int(int data_type, double data)
     if (data < INT_MIN || data > INT_MAX)
         return ("impossible");
     i = static_cast<int>(data);
-    if (data != i || data_type == UNKNOWN || data_type == MINFINITY || data_type == INFINITY || data_type == NAN)
+    if (data_type == UNKNOWN || data_type == MINFINITY || data_type == INFINITY || data_type == NAN)
         return ("impossible");
     ostr << i;
     return (ostr.str());
 }
 
-std::string print_float(int data_type, std::string value, double data)
+std::string print_float(int data_type, double data)
 {
     float f;
-    std::string buf = value;
     std::ostringstream ostr;
     std::string res;
 
-    if (data_type == FLOAT)
-    {
-        buf.push_back('f');
-        return (buf);
-    }
-    if (data < -FLT_MAX || data > FLT_MAX)
-        return ("impossible");
     f = static_cast<float>(data);
     if (data_type == NAN)
 		return ("nanf");
 	else if (data_type == INFINITY)
 		return ("inff");
-	else if (data_type == MINFINITY)
+    else if (data_type == MINFINITY)
 		return ("-inff");
+    else if (data < -FLT_MAX || data > FLT_MAX)
+        return ("impossible");
     ostr << f;
     res = ostr.str();
-    res += ".0";
+    if (is_int(res))
+        res += ".0";
     res += "f";
     return (res);
 }
+
 
 int main(int argc, char **argv)
 {
@@ -148,7 +154,7 @@ int main(int argc, char **argv)
     }
     std::cout << "char: " << print_char(data_type, data) << std::endl;
     std::cout << "int: " << print_int(data_type, data) << std::endl;
-    std::cout << "float: " << print_float(data_type, value, data) << std::endl;
-    std::cout << "double: " << print_double(data_type, value, data) << std::endl;
+    std::cout << "float: " << print_float(data_type, data) << std::endl;
+    std::cout << "double: " << print_double(data_type, data) << std::endl;
     return (0);
 }
